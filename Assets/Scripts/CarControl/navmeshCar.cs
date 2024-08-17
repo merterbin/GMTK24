a11266bb0be12d308f7d5af572536036680b3260
Görkem <kolaygorkem@icloud.com>
2024-08-18 02:00:00 +0300

    ADDED car navmesh added car navmesh and fixed problems

---
+++ Assets/Scripts/CarControl/navmeshCar.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMashCar : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    NavMeshAgent _navMashCar;
+    public int waypointsIndex = 1;
+    public Transform[] frontCheck;
+    public float speed = 10f;
+    public float stoppingDistance = 5f;
+
+    void Start()
+    {
+        _navMashCar = GetComponent<NavMeshAgent>();
+        if (waypoints.Length > 0)
+        {
+            _navMashCar.SetDestination(waypoints[waypointsIndex].transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goAroundwaypoints();
+        //checkDistance();
+    }
+    public void goAroundwaypoints()
+    {
+        if (_navMashCar.remainingDistance < 0.5f && !_navMashCar.pathPending)
+        {
+            if (waypointsIndex >= waypoints.Length)
+            {
+                waypointsIndex = 1;
+            }
+            else
+            {
+                waypointsIndex = waypointsIndex + 1;
+            }
+            _navMashCar.SetDestination(waypoints[waypointsIndex - 1].transform.position);
+
+        }
+    }
+    //void checkDistance()
+    //{
+    //    RaycastHit hit;
+    //    for (int i = 0; i < frontCheck.Length; i++)
+    //    {
+
+    //        if (Physics.Raycast(frontCheck[i].position, transform.forward, out hit, stoppingDistance))
+    //        {
+    //            Debug.Log("BULDU");
+    //            if (hit.collider.CompareTag("Car"))
+    //            {
+    //                Debug.Log("ARABA BULDU");
+    //                _navMashCar.speed = 0f;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            goAroundwaypoints();
+    //            _navMashCar.speed = 5f;
+    //        }
+    //    }
+    //}
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("ÇARPTI");
+    //    Debug.Log(collision.gameObject.tag);
+    //    if (collision.gameObject.CompareTag("Car"))
+    //    {
+    //        Debug.Log("ÝF ÇARPTI");
+    //        _navMashCar.speed = 0f;
+    //    }
+    //}
+
+}
