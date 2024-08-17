e63b5a725abf404b997fae5af78f7e00b21ba803
ErsaGunTosun <ersagunfatihdev@gmail.com>
2024-08-17 12:06:15 -0700

    ADD grid build system grid system bugs fixed and added grid build system but have bugs

--- Assets/Scripts/BuildingSystem/InputManager.cs
+++ Assets/Scripts/BuildingSystem/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,19 @@ public class InputManager : MonoBehaviour
 
     [SerializeField]
     private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
+
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
 
     public Vector3 GetSelectedMapPosition()
     {
